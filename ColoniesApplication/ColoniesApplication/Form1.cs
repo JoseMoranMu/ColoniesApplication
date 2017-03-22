3c0217b6304d7eb40b92cbc6eec311479679a5f4
JoseMoranMu <josemoranmu@gmail.com>
2017-03-22 12:18:43 +0100

    MVC creation
    MVC structure created

--- ColoniesApplication/ColoniesApplication/Form1.cs
+++ ColoniesApplication/ColoniesApplication/Form1.cs
@@ -16,5 +16,11 @@ namespace ColoniesApplication
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String u = textBox1.Text;
+            String p = textBox1.Text;
+        }
     }
 }
