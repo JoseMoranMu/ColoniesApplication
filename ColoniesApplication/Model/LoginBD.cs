66d40dc1768a31c06f80a693cb95fe70ddcfa490
JoseMoranMu <josemoranmu@gmail.com>
2017-03-22 13:01:36 +0100

    Login Class created

---
+++ ColoniesApplication/Model/LoginBD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    class LoginBD
+    {
+    }
+    public static bool login(String user, String password) {
+        bool b = false;
+
+        return b;
+    }
+}
