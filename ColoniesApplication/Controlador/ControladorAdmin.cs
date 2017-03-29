406c6057a978c6db15f5d6f007dd48a37b944a78
JoseMoranMu <josemoranmu@gmail.com>
2017-03-29 16:10:54 +0200

    Controladores creados parcialmente
    Falta definir el tipo de dato en monitor y admin

---
+++ ColoniesApplication/Controlador/ControladorAdmin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controlador
+{
+    class ControladorAdmin
+    {
+        ColoniesBD bd;
+        public ControladorAdmin(ColoniesBD bd)
+        {
+            this.bd = bd;
+        }
+        public bool insertatNens() {
+            return true;
+        }
+        public bool modificarNen() {
+            return true;
+        }
+        public bool insertarInscripcion() {
+            return true;
+        }
+        public bool modificarInscripcion(){
+            return true;
+        }
+    }
+}
---
+++ ColoniesApplication/Controlador/ControladorMonitor.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controlador
+{
+    class ControladorMonitor
+    {
+        ColoniesBD bd;
+        public ControladorMonitor(ColoniesBD bd)
+        {
+            this.bd = bd;
+        }
+        public bool registarTascaTanda() {
+            return true;
+        }
+    }
+}
